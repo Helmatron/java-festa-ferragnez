9e56da7d94ff19e8290f7094cd84ca012fe9eea0
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-04-27 19:55:54 +0200

    Esercizio finito

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 Consegna: creare un progetto java-festa-ferragnez con il package com.ferragnez.party, a cui aggiungere la classe CheckGuest
 Nel programma bisogna: creare e inizializzare l’array contenente i nomi degli invitati chiedere all’utente come si chiama e  verificare che il nome sia presente nella lista; lasciarlo entrare o rispedirlo cortesemente da dove è venuto :occhiali_da_sole:
 Bonus se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso programma usando il ciclo while, e viceversa Buon lavoro e buon weekend! :faccia_leggermente_sorridente: :tada: Lista invitati: Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+
+1-	creare e inizializzare l’array contenente i nomi degli invitati
+1.1-	Array Lista invitati: Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+
+2-	chiedere all’utente come si chiama
+2.1-	usare lo Scan per chiedere il nome
+
+3-	verificare che il nome sia presente nella lista
+	Uso un ciclo For per interrogare l'Array
+	Se il nome è presente: può entrare
+	Altrimenti: non può entrare	
+
+
+Cercando in rete ho trovato il metodo .equalsIgnoreCase che permette di mettere a confronto due o più valori all'interno dell'array, inoltre ignora le maiuscole e minuscole.
--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -1,9 +1,58 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		// << ARRAY >>
+		// Dichiaro e inizializzo un Array di stringhe con i nomi dei partecipanti
+		String[] nomiPartecipanti = {
+		        "Dua Lipa",
+		        "Paris Hilton",
+		        "Manuel Agnelli",
+		        "J-Ax",
+		        "Francesco Totti",
+		        "Ilary Blasi",
+		        "Bebe Vio",
+		        "Luis",
+		        "Pardis Zarei",
+		        "Martina Maccherone",
+		        "Rachel Zeilic"
+				};
+
+		
+		// << INPUT TASTIERA >>
+			// Attivo la raccolta degli input da tastiera col metodo (System.in)
+			Scanner input = new Scanner(System.in);
+
+			
+		// << NOME >>
+			// Stampo la richiesta del Nome all'utente
+			System.out.println("Inserisci il tuo Nome");
+			// Imposto variabile NOME e richiedo il valore da console - String
+			String name = input.nextLine();
+
+			
+		// << CICLO DI VERIFICA >>
+			// Dichiaro una variabile booleana per indicare se il nome è stato trovato
+				boolean found = false; 
+
+		// Controlla se il nome dell'utente esiste nell'array (non sensibile alle maiuscole/minuscole)
+			for (String participantName : nomiPartecipanti) {
+			    if (participantName.equalsIgnoreCase(name)) {
+			        found = true;
+			        break; // Ferma il ciclo For se il nome viene trovato
+				    }
+				}
+		
+				if (found) {
+				    System.out.println("puoi entrare");
+				} else {
+				    System.out.println("non puoi entrare");
+				}
 
 	}
 
